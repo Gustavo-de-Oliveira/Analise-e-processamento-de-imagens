4fdec9dce391e9957605b4d96429dc44e555d708
Gustavo-de-OLiveira <gustavo_de_oliveira@usp.br>
2020-03-26 20:18:22 -0300

    Versão 1

---
+++ filtroGabor-1.py
@@ -0,0 +1,61 @@
+# opencv
+import cv2
+import numpy as np
+
+def gabor(sigma, theta, Lambda, psi, gamma):
+    """Gabor feature extraction."""
+    sigma_x = sigma
+    sigma_y = float(sigma) / gamma
+
+    # Bounding box
+    nstds = 3  # Number of standard deviation sigma
+    xmax = max(abs(nstds * sigma_x * np.cos(theta)), abs(nstds * sigma_y * np.sin(theta)))
+    xmax = np.ceil(max(1, xmax))
+    ymax = max(abs(nstds * sigma_x * np.sin(theta)), abs(nstds * sigma_y * np.cos(theta)))
+    ymax = np.ceil(max(1, ymax))
+    xmin = -xmax
+    ymin = -ymax
+    (y, x) = np.meshgrid(np.arange(ymin, ymax + 1), np.arange(xmin, xmax + 1))
+
+    # Rotation
+    x_theta = x * np.cos(theta) + y * np.sin(theta)
+    y_theta = -x * np.sin(theta) + y * np.cos(theta)
+
+    gb = np.exp(-.5 * (x_theta ** 2 / sigma_x ** 2 + y_theta ** 2 / sigma_y ** 2)) * np.cos(2 * np.pi / Lambda * x_theta + psi)
+    return gb
+
+def build_filters():
+    """ returns a list of kernels in several orientations
+    """
+    filters = []
+    ksize = 31
+    for theta in np.arange(0, np.pi, np.pi / 32):
+        #filtro = gabor(1, 0, 6.5, -1.5, 0.3)
+        params = {'sigma':1.0, 'theta':theta, 'Lambda':10,
+                  'psi':2, 'gamma':0.5}
+        kern = gabor(**params)
+        kern /= 1.5*kern.sum()
+        filters.append((kern,params))
+    return filters
+
+def process(img, filters):
+    """ returns the img filtered by the filter list
+    """
+    accum = np.zeros_like(img)
+    for kern,params in filters:
+        fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
+        np.maximum(accum, fimg, accum)
+    return accum
+
+#main
+filters = build_filters()
+
+img = cv2.imread('img2-normalizada.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('image', img)
+
+filtered_img = process(img, filters)
+
+cv2.imshow('filtered image', filtered_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
